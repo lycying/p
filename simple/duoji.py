5f852e13e460f7a736b33a8bdaa692e3a4d8aed6
yadongx <yadongx@gmail.com>
2016-01-31 01:57:33 +0000

    ok

--- simple/duoji.py
+++ simple/duoji.py
@@ -7,7 +7,7 @@ import atexit
 atexit.register(GPIO.cleanup)  
 
 servopin = 23
-servopin2 = 14
+servopin2 = 25
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servopin, GPIO.OUT, initial=False)
@@ -16,25 +16,44 @@ p = GPIO.PWM(servopin,50)
 p2 = GPIO.PWM(servopin2,50)
 p.start(0)
 p2.start(0)
+p.ChangeDutyCycle(0)
+p2.ChangeDutyCycle(0)
 time.sleep(2)
 
 while(True):
     for i in range(0,181,10):
-        p.ChangeDutyCycle(2.5 + 10 * i / 180)
-        time.sleep(0.01) 
-        print("wa")
-
-    for i in range(181,0,-10):
-        p.ChangeDutyCycle(2.5 + 10 * i / 180)
-        time.sleep(0.01)
-        print(".\c")
-
-    for i in range(0,181,10):
         p2.ChangeDutyCycle(2.5 + 10 * i / 180)
-        time.sleep(0.01) 
+        time.sleep(0.1) 
         print("wa2")
 
+    p.ChangeDutyCycle(0)
+    p2.ChangeDutyCycle(0)
+    time.sleep(2)
+
     for i in range(181,0,-10):
         p2.ChangeDutyCycle(2.5 + 10 * i / 180)
-        time.sleep(0.01)
+        time.sleep(0.1)
         print(".\c2")
+
+    p.ChangeDutyCycle(0)
+    p2.ChangeDutyCycle(0)
+    time.sleep(2)
+
+    for i in range(0,181,10):
+        p.ChangeDutyCycle(2.5 + 10 * i / 180)
+        time.sleep(0.1) 
+        print("wa")
+
+    p.ChangeDutyCycle(0)
+    p2.ChangeDutyCycle(0)
+    time.sleep(2)
+
+    for i in range(181,0,-10):
+        p.ChangeDutyCycle(2.5 + 10 * i / 180)
+        time.sleep(0.1)
+        print(".\c")
+
+    p.ChangeDutyCycle(0)
+    p2.ChangeDutyCycle(0)
+    time.sleep(2)
+    
--- simple/ss.py
+++ simple/ss.py
@@ -1,11 +1,78 @@
 import serial
-ser = serial.Serial('/dev/ttyAMA0', 38400, timeout=1)
+import RPi.GPIO as GPIO
+import time
+import signal
+import atexit
+
+atexit.register(GPIO.cleanup)  
+
+servopin = 23
+servopin2 = 25
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servopin, GPIO.OUT, initial=False)
+GPIO.setup(servopin2, GPIO.OUT, initial=False)
+p = GPIO.PWM(servopin,50)
+p2 = GPIO.PWM(servopin2,50)
+p.start(0)
+p2.start(0)
+p.ChangeDutyCycle(0)
+p2.ChangeDutyCycle(0)
+
+ser = serial.Serial('/dev/ttyAMA0', 9600 , timeout=1)
 ser.close()
 ser.open()
-ser.write("light on\n")
+ser.write("ready\r\n")
+
+
+p2_ar = 0
+p_ar = 0
+
 try:
     while 1:
-        response = ser.readline()
-        print response
+        resp = ser.readline().replace("\r\n","")
+        if resp == "a1":
+            p2_ar = p2_ar + 10
+            if p2_ar < 181:
+                ser.write("turn to left\r\n")
+                p2.ChangeDutyCycle(2.5 + 10 * p2_ar / 180)
+                time.sleep(0.1) 
+                p2.ChangeDutyCycle(0)
+            else:
+                p2_ar = p2_ar - 10
+                ser.write("left out of range \r\n")
+
+        elif resp == "a2":
+            p2_ar = p2_ar - 10
+            if p2_ar > -1:
+                ser.write("turn to right\r\n")
+                p2.ChangeDutyCycle(2.5 + 10 * p2_ar/ 180)
+                time.sleep(0.1) 
+                p2.ChangeDutyCycle(0)
+            else:
+                p2_ar = p2_ar + 10
+                ser.write("right out of range\r\n")
+
+        elif resp == "b1":
+            p_ar = p_ar + 10
+            if p_ar < 181:
+                ser.write("turn to up\r\n")
+                p.ChangeDutyCycle(2.5 + 10 * p_ar / 180)
+                time.sleep(0.1) 
+                p.ChangeDutyCycle(0)
+            else:
+                p_ar = p_ar - 10
+                ser.write("up out of range \r\n")
+        elif resp == "b2":
+            p_ar = p_ar - 10
+            if p_ar > -1:
+                ser.write("turn to down\r\n")
+                p.ChangeDutyCycle(2.5 + 10 * p_ar/ 180)
+                time.sleep(0.1) 
+                p.ChangeDutyCycle(0)
+            else:
+                p_ar = p_ar + 10
+                ser.write("down out of range\r\n")
+
 except KeyboardInterrupt:
     ser.close()
